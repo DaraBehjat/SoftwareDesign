f976667662fefc7ff050bb7ab476a523eb4a02d5
Dara <dara@dara-ThinkPad-T431s.(none)>
2013-10-30 18:27:51 -0400

    Dara's Homework 3

--- chap03/grid.py
+++ chap03/grid.py
@@ -1,6 +1,31 @@
-"""Solution to an exercise in Think Python.
+def do_twice(f):
+    f()
+    f()
 
-Author:
-"""
+def do_four(f):
+    do_twice(f)
+    do_twice(f)
 
-print '+ - - - -'
+def print_bar():
+    print '+ - - - -',
+
+def print_post():
+    print '|        ',
+
+def print_bars():
+    do_twice(print_bar)
+    print '+'
+
+def print_posts():
+    do_twice(print_post)
+    print '|'
+
+def print_row():
+    print_bars()
+    do_four(print_posts)
+
+def print_grid():
+    do_twice(print_row)
+    print_bars()
+
+print_grid()
